63547f741c528a8f952519917b88f91bec4b26ac
Siddhant Prakash <sid.rol92@gmail.com>
2017-11-30 00:18:43 -0700

    pre-final updates

--- code/experiments.py
+++ code/experiments.py
@@ -58,7 +58,8 @@ def cross_validation(X, y, clf, thr):
 
 
 if __name__ == "__main__":
-    dataset = "pimaindians"
+    datasetNames = ['mammography', 'sat', 'pimaindians']
+    dataset = datasetNames[2]
     data = smote.dataRead(dataset+".csv")
     print data.shape
 
@@ -67,7 +68,7 @@ if __name__ == "__main__":
     majority_underSample_percent = [10, 15, 25, 50, 75, 100, 125, 150, 175, 200, 300, 400, 500, 600, 700, 800, 1000,
                                     2000]
 
-    visSample = 3
+    visSample = 1
     NBthresh = 0.5
     thresh = 1.0
     KNN = 5
@@ -121,123 +122,124 @@ if __name__ == "__main__":
     print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
 
     for j in range(len(minority_overSample_percent)):
-        if j == visSample:
-            for i in range(len(majority_underSample_percent)):
-                print '#######################################################################'
-                dataB = smote.underSMOTE(data, majority_underSample_percent[i])
+        # if j == visSample:
+        for i in range(len(majority_underSample_percent)):
+            print '#######################################################################'
+            dataB = smote.underSMOTE(data, majority_underSample_percent[i])
 
-                unique, counts = np.unique(dataB[:, [data.shape[1] - 1]], return_counts=True)
-                freq = dict(zip(unique, counts))
-                nPositive = freq[1.0]
-                nNegative = freq[0.0]
-                print 'UnderSampled by ' + str(majority_underSample_percent[i]) + ' %'
-                print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+            unique, counts = np.unique(dataB[:, [data.shape[1] - 1]], return_counts=True)
+            freq = dict(zip(unique, counts))
+            nPositive = freq[1.0]
+            nNegative = freq[0.0]
+            print 'UnderSampled by ' + str(majority_underSample_percent[i]) + ' %'
+            print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
 
-                print '------------------------------------------------------------'
+            print '------------------------------------------------------------'
 
-                # Decision Tree classifier on Plain Under-sampled data
-                X = dataB[:, [1, 0]]
-                y = dataB[:, [data.shape[1] - 1]]
+            # Decision Tree classifier on Plain Under-sampled data
+            X = dataB[:, [1, 0]]
+            y = dataB[:, [data.shape[1] - 1]]
 
-                clf = DecisionTreeClassifier()
-                fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
-                expB[j, i+1, :] = [fpr, tpr]
+            clf = DecisionTreeClassifier()
+            fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
+            expB[j, i+1, :] = [fpr, tpr]
 
-                # Gaussian Naive Bayes classifier on Plain Under-sampled data
-                clf = GaussianNB()
-                fpr, tpr = cross_validation(X, y[:, 0], clf, NBthresh)
-                print 'Gauss NB Class Priors [-ve class, +ve class]: ', clf.class_prior_
-                priors.append(clf.class_prior_)
-                expC[j, i + 1, :] = [fpr, tpr]
+            # Gaussian Naive Bayes classifier on Plain Under-sampled data
+            clf = GaussianNB()
+            fpr, tpr = cross_validation(X, y[:, 0], clf, NBthresh)
+            print 'Gauss NB Class Priors [-ve class, +ve class]: ', clf.class_prior_
+            priors.append(clf.class_prior_)
+            expC[j, i + 1, :] = [fpr, tpr]
 
-                # k-NN classifier on Plain Under-sampled data
-                clf = KNeighborsClassifier(n_neighbors=KNN)
-                fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
-                expE[j, i + 1, :] = [fpr, tpr]
+            # k-NN classifier on Plain Under-sampled data
+            clf = KNeighborsClassifier(n_neighbors=KNN)
+            fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
+            expE[j, i + 1, :] = [fpr, tpr]
 
-                dataC = smote.smote(data, minority_overSample_percent[j], 5)
+            dataC = smote.smote(data, minority_overSample_percent[j], 5)
 
-                X = dataC[:, [1, 0]]
-                y = dataC[:, [data.shape[1] - 1]]
+            X = dataC[:, [1, 0]]
+            y = dataC[:, [data.shape[1] - 1]]
 
-                unique, counts = np.unique(dataC[:, [data.shape[1] - 1]], return_counts=True)
-                freq = dict(zip(unique, counts))
-                nPositive = freq[1.0]
-                nNegative = freq[0.0]
-                print 'SMOTED by '+str(minority_overSample_percent[j])+' %'
-                print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+            unique, counts = np.unique(dataC[:, [data.shape[1] - 1]], return_counts=True)
+            freq = dict(zip(unique, counts))
+            nPositive = freq[1.0]
+            nNegative = freq[0.0]
+            print 'SMOTED by '+str(minority_overSample_percent[j])+' %'
+            print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
 
-                dataC = smote.underSMOTE(dataC, majority_underSample_percent[i])
+            dataC = smote.underSMOTE(dataC, majority_underSample_percent[i])
 
-                unique, counts = np.unique(dataC[:, [data.shape[1] - 1]], return_counts=True)
-                freq = dict(zip(unique, counts))
-                nPositive = freq[1.0]
-                nNegative = freq[0.0]
-                print 'UnderSMOTED by '+str(majority_underSample_percent[i])+' %'
-                print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
-                print '------------------------------------------------------------'
+            unique, counts = np.unique(dataC[:, [data.shape[1] - 1]], return_counts=True)
+            freq = dict(zip(unique, counts))
+            nPositive = freq[1.0]
+            nNegative = freq[0.0]
+            print 'UnderSMOTED by '+str(majority_underSample_percent[i])+' %'
+            print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+            print '------------------------------------------------------------'
 
-                X = dataC[:, [1, 0]]
-                y = dataC[:, [data.shape[1] - 1]]
+            X = dataC[:, [1, 0]]
+            y = dataC[:, [data.shape[1] - 1]]
 
-                # Decision Tree classifier on SMOTE+Under-sampled data
-                clf = DecisionTreeClassifier()
-                fpr, tpr = cross_validation(X,y[:,0], clf, thresh)
-                expA[j, i+1, :] = [fpr, tpr]
+            # Decision Tree classifier on SMOTE+Under-sampled data
+            clf = DecisionTreeClassifier()
+            fpr, tpr = cross_validation(X,y[:,0], clf, thresh)
+            expA[j, i+1, :] = [fpr, tpr]
 
-                # k-NN classifier on SMOTE+Under-sampled data
-                clf = KNeighborsClassifier(n_neighbors=KNN)
-                fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
-                expD[j, i+1, :] = [fpr, tpr]
+            # k-NN classifier on SMOTE+Under-sampled data
+            clf = KNeighborsClassifier(n_neighbors=KNN)
+            fpr, tpr = cross_validation(X, y[:, 0], clf, thresh)
+            expD[j, i+1, :] = [fpr, tpr]
 
-                print '#######################################################################'
+            print '#######################################################################'
 
-    expA[:, -1, :] = 1.0
-    expB[:, -1, :] = 1.0
-    expC[:, -1, :] = 1.0
-    expD[:, -1, :] = 1.0
-    expE[:, -1, :] = 1.0
-    expF[:, -1, :] = 1.0
+        expA[:, -1, :] = 1.0
+        expB[:, -1, :] = 1.0
+        expC[:, -1, :] = 1.0
+        expD[:, -1, :] = 1.0
+        expE[:, -1, :] = 1.0
+        expF[:, -1, :] = 1.0
+        visSample = j
 
-    cHullArr1 = np.concatenate((expA[visSample, :, :], expB[visSample, :, :], expC[visSample, :, :]))
-    hull1 = ConvexHull(cHullArr1)
+        cHullArr1 = np.concatenate((expA[visSample, :, :], expB[visSample, :, :], expC[visSample, :, :]))
+        hull1 = ConvexHull(cHullArr1)
 
-    plt.plot(expA[visSample, :, 0], expA[visSample, :, 1], 'b+', linestyle='-', label=r'SMOTE 400%')
-    plt.plot(expB[visSample, :, 0], expB[visSample, :, 1], 'ro', linestyle='-', label=r'Under')
-    plt.plot(expC[visSample, :, 0], expC[visSample, :, 1], 'k^', linestyle='-', label=r'Naive Bayes')
-    plt.plot(1.0 , 1.0, 'g*', linestyle='-.', label=r'Convex Hull')
-    for simplex in hull1.simplices:
-        plt.plot(cHullArr1[simplex, 0], cHullArr1[simplex, 1], 'g*', linestyle='-.')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic curve - Decision Tree on '+dataset+' dataset')
-    plt.legend(loc="lower right")
-    plt.show()
-    # plt.savefig(dataset+'_ROC1.png')
-    plt.close()
+        plt.plot(expA[visSample, :, 0], expA[visSample, :, 1], 'b+', linestyle='-', label=r'SMOTE '+str(minority_overSample_percent[visSample])+'%')
+        plt.plot(expB[visSample, :, 0], expB[visSample, :, 1], 'ro', linestyle='-', label=r'Under')
+        plt.plot(expC[visSample, :, 0], expC[visSample, :, 1], 'k^', linestyle='-', label=r'Naive Bayes')
+        plt.plot(1.0 , 1.0, 'g*', linestyle='-.', label=r'Convex Hull')
+        for simplex in hull1.simplices:
+            plt.plot(cHullArr1[simplex, 0], cHullArr1[simplex, 1], 'g*', linestyle='-.')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic curve - Decision Tree on '+dataset+' dataset')
+        plt.legend(loc="lower right")
+        # plt.show()
+        plt.savefig('figures/'+dataset+'/ROC_1_'+str(minority_overSample_percent[visSample])+'.png')
+        plt.close()
 
-    cHullArr2 = np.concatenate((expD[visSample, :, :], expE[visSample, :, :]))
-    hull2 = ConvexHull(cHullArr2)
+        cHullArr2 = np.concatenate((expD[visSample, :, :], expE[visSample, :, :]))
+        hull2 = ConvexHull(cHullArr2)
 
-    plt.plot(expD[visSample, :, 0], expD[visSample, :, 1], 'b+', linestyle='-', label=r'SMOTE 400%')
-    plt.plot(expE[visSample, :, 0], expD[visSample, :, 1], 'ro', linestyle='-', label=r'Under')
-    plt.plot(1.0, 1.0, 'g*', linestyle='-.', label=r'Convex Hull')
-    for simplex in hull2.simplices:
-        plt.plot(cHullArr2[simplex, 0], cHullArr2[simplex, 1], 'g*', linestyle='-.')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic curve - '+str(KNN)+'-NN on ' + dataset+' dataset')
-    plt.legend(loc="lower right")
-    plt.show()
-    # plt.savefig(dataset+'_ROC2.png')
-    plt.close()
+        plt.plot(expD[visSample, :, 0], expD[visSample, :, 1], 'b+', linestyle='-', label=r'SMOTE '+str(minority_overSample_percent[visSample])+'%')
+        plt.plot(expE[visSample, :, 0], expD[visSample, :, 1], 'ro', linestyle='-', label=r'Under')
+        plt.plot(1.0, 1.0, 'g*', linestyle='-.', label=r'Convex Hull')
+        for simplex in hull2.simplices:
+            plt.plot(cHullArr2[simplex, 0], cHullArr2[simplex, 1], 'g*', linestyle='-.')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic curve - '+str(KNN)+'-NN on ' + dataset+' dataset')
+        plt.legend(loc="lower right")
+        # plt.show()
+        plt.savefig('figures/'+dataset+'/ROC_2_'+str(minority_overSample_percent[visSample])+'.png')
+        plt.close()
 
-    print '-----------------------------------'
-    print 'Priors: ', priors
+        print '-----------------------------------'
+        print 'Priors: ', priors
 
         # scores = cross_val_score(clfUSmote, X, Y[:,0], cv=10)
         # # print scores.shape
---
+++ code/experimentsAUC.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import numpy as np
+import smote
+from scipy import interp
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from scipy.spatial import ConvexHull
+from sklearn.metrics import roc_curve, auc
+
+def cross_validation(X, y, clf, thr):
+    # 10-fold cross-validation
+    cv = StratifiedKFold(n_splits=10)
+
+
+    tprs = []
+    aucs=[]
+    mean_fpr = np.linspace(0, 1, 100)
+
+
+    for train, test in cv.split(X, y):
+        probas_ = clf.fit(X[train], y[train]).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        roc_auc=auc(fpr,tpr)
+        aucs.append(roc_auc)
+        # k = 0
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_auc = auc(mean_fpr, mean_tpr)
+
+    return mean_auc, mean_fpr, mean_tpr
+
+
+
+if __name__ == "__main__":
+    datasetNames = ['mammography', 'sat', 'pimaindians']
+    dataset = datasetNames[0]
+    data = smote.dataRead(dataset+".csv")
+
+    minority_overSample_percent = [100, 200, 300, 400, 500]
+    majority_underSample_percent = [10, 100, 150, 200, 500, 1000]
+    # majority_underSample_percent = [10, 15, 25, 50, 75, 100, 125, 150, 175, 200, 300, 400, 500, 600, 700, 800, 1000,
+    #                                 2000]
+
+    visSample = 3
+    NBthresh = 0.5
+    thresh = 1.0
+    KNN = 5
+    # minority_overSample_percent = [400]
+    # majority_underSample_percent = [100, 300]
+
+    pca_true = False
+    if pca_true:
+        xData = data[:, :data.shape[1]-1]
+        yData = data[:, [data.shape[1]-1]]
+
+        pca = PCA(n_components=2)
+        xData = pca.fit_transform(xData)
+        # print xData.shape
+
+        data = np.empty((data.shape[0], 3), dtype=xData.dtype)
+        data[: , :2] = xData
+        data[:, [2]] = yData
+
+    X = data[:, [1, 0]]
+    y = data[:, [data.shape[1] - 1]]
+
+    exp = np.empty((len(majority_underSample_percent), len(minority_overSample_percent)+1))
+
+
+    priors = []
+
+    exp[:, 0] = 0
+
+
+
+    unique, counts = np.unique(data[:, [data.shape[1] - 1]], return_counts=True)
+    freq = dict(zip(unique, counts))
+    nPositive = freq[1.0]
+    nNegative = freq[0.0]
+    # print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+
+    for i in range(len(majority_underSample_percent)):
+        # print '#######################################################################'
+        dataB = smote.underSMOTE(data, majority_underSample_percent[i])
+
+        unique, counts = np.unique(dataB[:, [data.shape[1] - 1]], return_counts=True)
+        freq = dict(zip(unique, counts))
+        nPositive = freq[1.0]
+        nNegative = freq[0.0]
+        print 'UnderSampled by ' + str(majority_underSample_percent[i]) + ' %'
+        # print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+
+        # Decision Tree classifier on Under-sampled majority data
+        X = dataB[:, [1, 0]]
+        y = dataB[:, [data.shape[1] - 1]]
+
+        clf = DecisionTreeClassifier()
+        aucv,fpr,tpr = cross_validation(X, y[:, 0], clf, thresh)
+        exp[i,0] = aucv
+        print 'SMOTED by ',
+        for j in range(len(minority_overSample_percent)):
+            # print '------------------------------------------------------------'
+            #Over-sampled minority data on undersampled Majority Data
+            dataC = smote.smote(dataB, minority_overSample_percent[j], 5)
+
+            X = dataC[:, [1, 0]]
+            y = dataC[:, [data.shape[1] - 1]]
+
+            unique, counts = np.unique(dataC[:, [dataC.shape[1] - 1]], return_counts=True)
+            freq = dict(zip(unique, counts))
+            nPositive = freq[1.0]
+            nNegative = freq[0.0]
+            print str(minority_overSample_percent[j])+' %, ',
+            # print '+ve Class: ', nPositive, ' -ve Class: ', nNegative
+
+
+            # Decision Tree classifier on SMOTE+Under-sampled data
+            clf = DecisionTreeClassifier()
+            aucv, fpr, tpr  = cross_validation(X,y[:,0], clf, thresh)
+            exp[i, j+1] = aucv
+
+    print dataset
+    print np.mean(exp,axis=0);
+    print "####################################################################"
+    print exp
+
+    '''Sample Output'''
+    '''
+             Undersample       100% 	 200%        300%	      400%		 500%
+    SatImage [ 0.78659246  0.78810681  0.82591577  0.83178657  0.83804842  0.82834139]
+    Mammo    [ 0.72201504  0.72897763  0.77764215  0.79193469  0.79187588  0.77518559]
+    pima     [ 0.65814065  0.65670094  0.7014472   0.71445631  0.72072788  0.73268711]
+    '''
